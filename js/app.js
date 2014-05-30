8810bc93d0fb39169b4b1d411e9e12d603db09c1
Gundi <goncalo.neto@webdetails.pt>
2014-05-30 16:29:18 +0100

    Refactored call to marketplace metadata so that it is wrapped in an angular service.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
-(function() {
 
-	var markteplacePluginsXMLUrl = 'marketplace.xml';
+var app = angular.module('marketplace', ['ngRoute']);
 
-	$.get(markteplacePluginsXMLUrl, function(xml ){ 
-		var pluginsMetadata = $.xml2json(xml); 
-		//console.log(marketplacePlugins);
-	
-		var app = angular.module('marketplace', ['ngRoute']);
+app.value('MarketplaceConfig',{
+	xmlUrl: 'marketplace.xml'
+});
 
-		app.controller('MarketplaceController', function(){
-			this.marketplacePlugins = pluginsMetadata; 
-		});
-	});	
-})();
+app.factory('PluginsMetadata', function($http, MarketplaceConfig){
+	return {
+		getMetadata: function(){
+			return $http.get( MarketplaceConfig.xmlUrl ).then( function(result){
+				return $.xml2json(result.data).market_entry;
+			});
+		}
+	}
+});
+
+app.controller('MarketplaceController', function( $scope, PluginsMetadata ){
+	$scope.pluginsList = [];
+	$scope.searchTerm = "";
+
+	PluginsMetadata.getMetadata().then(function(data){
+		$scope.pluginsList = data;
+	}); 
+});
