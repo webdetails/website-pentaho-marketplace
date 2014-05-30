fff6e13cbf5e3acc2edb5bcecb9248e91a30feee
Miguel Leite <miguelacleite@gmail.com>
2014-05-30 17:46:57 +0100

    added function to cut long strings, changed plugin search to search through all parameters, added plugin screenshot, description and author/author url

--- js/app.js
+++ js/app.js
@@ -23,3 +23,23 @@ app.controller('MarketplaceController', function( $scope, PluginsMetadata ){
 		$scope.pluginsList = data;
 	}); 
 });
+
+angular.module('ng').filter('cut', function () {
+    return function (value, wordwise, max, tail) {
+        if (!value) return '';
+
+        max = parseInt(max, 10);
+        if (!max) return value;
+        if (value.length <= max) return value;
+
+        value = value.substr(0, max);
+        if (wordwise) {
+            var lastspace = value.lastIndexOf(' ');
+            if (lastspace != -1) {
+                value = value.substr(0, lastspace);
+            }
+        }
+
+        return value + (tail || ' …');
+    };
+});
