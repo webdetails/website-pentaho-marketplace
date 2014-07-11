24d43a623c778370d8b68cd6f9150a359ed93d72
Miguel Leite <miguelacleite@gmail.com>
2014-07-11 17:20:18 +0100

    fixed images to blue hover states, other css fixes

--- js/main.js
+++ js/main.js
@@ -37,9 +37,9 @@ $('.plugins-filters .single-filter-button').click(function() {
 });
 
 $('.clear-all-filters').click(function() {
-	$('.plugins-filters  .single-filter-button').attr('data-checked', 'false');
-	$('.plugins-filters  #button-filters-show-all').attr('data-checked', 'true');
-	$('.plugins-filters  #button-filters-all-apps').attr('data-checked', 'true');
+	$('.plugins-filters .single-filter-button').attr('data-checked', 'false');
+	$('.plugins-filters .filters-state').removeClass('some-checked');
+	$('.plugins-filters .filters-state').removeClass('all-checked');
 });
 
 $('#inputSearch').focus(function() {
