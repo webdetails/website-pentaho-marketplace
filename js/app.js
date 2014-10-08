a26f4793d0bd1bbcf8a2c82248b24b29cd6ad552
Miguel Leite <miguelacleite@gmail.com>
2014-10-08 18:38:20 +0100

    - fixed dialog overlay

--- js/app.js
+++ js/app.js
@@ -204,6 +204,11 @@ app.controller('MarketplaceController',
             var dialogContentHeight = $dialog.find('.ngdialog-content').height();
             var dialogContentTotalHeight = dialogContentHeight + 100;
             $dialog.find('.ngdialog-overlay').css('min-height', dialogContentTotalHeight);
+            setTimeout(function() {
+              var dialogContentHeight = $dialog.find('.ngdialog-content').height();
+              var dialogContentTotalHeight = dialogContentHeight + 100;
+              $dialog.find('.ngdialog-overlay').css('min-height', dialogContentTotalHeight);
+            }, 500);
             console.log('ngDialog opened: ' + $dialog.attr('id') + '; ngDialog height: ' + dialogContentHeight + '; ngDialog height with margins: ' + dialogContentTotalHeight);
           }, 10);
         });
