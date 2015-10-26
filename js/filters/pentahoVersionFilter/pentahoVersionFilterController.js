e2a4f8d4864eeb213ddfaca3297bffdad58c0fbf
Miguel Leite <miguelacleite@gmail.com>
2015-10-26 11:32:33 +0000

    Added Pentaho 6.0 version to versions selector and set it as default.

--- js/filters/pentahoVersionFilter/pentahoVersionFilterController.js
+++ js/filters/pentahoVersionFilter/pentahoVersionFilterController.js
@@ -18,9 +18,9 @@
       ['$scope',
         function ( $scope ) {
 
-          $scope.versions = [ "4.8", "5.0", "5.1", "5.2", "5.3", "5.4" ];
+          $scope.versions = [ "4.8", "5.0", "5.1", "5.2", "5.3", "5.4", "6.0" ];
 
-          $scope.selectedVersion = "5.4";
+          $scope.selectedVersion = "6.0";
 
           $scope.selectVersion = function ( version ) {
             $scope.selectedVersion = version;
